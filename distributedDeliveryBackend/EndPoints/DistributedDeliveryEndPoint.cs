d167c89e2621fb81debe2c42f95f96ef66fe99f9
marcoquadrini <marco.quadrini@studenti.unicam.it>
2024-09-21 15:28:40 +0200

    fix documentation

--- distributedDeliveryBackend/EndPoints/DistributedDeliveryEndPoint.cs
+++ distributedDeliveryBackend/EndPoints/DistributedDeliveryEndPoint.cs
@@ -10,19 +10,24 @@ namespace distributedDeliveryBackend.EndPoints;
 
 public static class DistributedDeliveryEndPoint
 {
-
+    private const string riderTag = "RIDER"; 
+    private const string ordertag = "ORDER"; 
     public static WebApplication MapDistributedDeliveryEndpoints(this WebApplication app)
     {
-        
-        app.MapPost("/addOrder", (IGrainFactory grainFactory,OrderEventPublisher publisher, [FromBody] AddOrderRequest order) =>
+
+    
+   
+        app.MapPost("/addOrder", async (IGrainFactory grainFactory,OrderEventPublisher publisher, [FromBody] AddOrderRequest order) =>
             {
                 var customer = grainFactory.GetGrain<ICustomerGrain>(order.IdCustomer);
-                var orderId =  customer.CreateOrder(order.IdArticles);
+                await customer.CreateOrder(order.IdArticles);
                 publisher.PublishOrderCreatedEvent(order);
                 return order.DeliveryDetails.Name;
             })
+            .WithTags(ordertag)
             .WithOpenApi();
         
+     
         app.MapPost("/changeOrderStatus", async (IGrainFactory grainFactory, OrderEventPublisher publisher, [FromBody] ChangeOrderStatusRequest request) =>
         {
             var orderGrain = grainFactory.GetGrain<IOrderGrain>(request.idOrder);
@@ -33,21 +38,22 @@ public static class DistributedDeliveryEndPoint
                 return Results.Ok($"Order id: {request.idOrder} updated to status {request.newOrderState}");
             }
             return Results.Problem("Something went wrong updating the order status.");
-        });
+        })
+        .WithTags(ordertag)
+        .WithOpenApi();
 
 
-        
-        
+
         app.MapGet("/getOrder", async (IGrainFactory grainFactory, string idOrder) =>
-        {
-            var orderGrain = grainFactory.GetGrain<IOrderGrain>(idOrder);
-            var orderJson = await orderGrain.GetItem();
-            return orderJson;
-        });
+            {
+                var orderGrain = grainFactory.GetGrain<IOrderGrain>(idOrder);
+                var orderJson = await orderGrain.GetItem();
+                return orderJson;
+            })
+            .WithTags(ordertag)
+            .WithOpenApi();
         
-        
-        
-        //FATTO
+    
         app.MapPost("/registerRider", async (IGrainFactory grainFactory, RiderEventPublisher publisher, [FromBody] AddRiderRequest request) =>
             {
                 var newRiderId = Guid.NewGuid().ToString();
@@ -64,27 +70,36 @@ public static class DistributedDeliveryEndPoint
                 return "Something wrong";
                 
             })
+            .WithTags(riderTag)
             .WithOpenApi();
-        //FATTO
-        app.MapPost("/setWorkingRider", async (IGrainFactory grainFactory,RiderEventPublisher publisher, [FromBody] SetWorkingRiderRequest request) =>
-        {
-            var riderGrain = grainFactory.GetGrain<IRiderGrain>(request.RiderId);
-            var setWorkingRiderSuccess = await riderGrain.SetWorking(request.IsWorking);
-            if (setWorkingRiderSuccess)
+
+        app.MapPost("/setWorkingRider", async (IGrainFactory grainFactory, RiderEventPublisher publisher,
+                [FromBody] SetWorkingRiderRequest request) =>
             {
-                publisher.PublishSetWorkingRider(request);
-                return "Ok";
-            }
-            return "Something wrong";
-        });
-        
-        //Fatto
+                var riderGrain = grainFactory.GetGrain<IRiderGrain>(request.RiderId);
+                var setWorkingRiderSuccess = await riderGrain.SetWorking(request.IsWorking);
+                if (setWorkingRiderSuccess)
+                {
+                    publisher.PublishSetWorkingRider(request);
+                    return "Ok";
+                }
+
+                return "Something wrong";
+            })
+            .WithTags(riderTag)
+            .WithOpenApi();
+
         app.MapGet("/getIsWorking", async (IGrainFactory grainFactory, string idRider) =>
-        {
-            var riderGrain = grainFactory.GetGrain<IRiderGrain>(idRider);
-            var orderJson = await riderGrain.IsWorking();
-            return orderJson;
-        });
+            {
+                var riderGrain = grainFactory.GetGrain<IRiderGrain>(idRider);
+                var orderJson = await riderGrain.IsWorking();
+                return orderJson;
+            })
+            .WithTags(riderTag)
+            .WithOpenApi();
+        
+        
         return app;
+        
     }
 }
